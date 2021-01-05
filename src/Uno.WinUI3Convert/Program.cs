8c5ce2736d4a2652472d9755e7abe8a503834355
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2021-01-05 16:25:45 -0500

    feat: Add project files.

---
+++ src/Uno.WinUI3Convert/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Uno.WinUI3Convert
+{
+    class Program
+    {
+        static Task<int> Main(string[] args)
+        {
+            var command = new RootCommand("Migrate UWP projects to WinUI")
+            {
+                new Argument<DirectoryInfo>("source", "Source directory")
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+                .ExistingOnly(),
+
+                new Argument<DirectoryInfo>("destination", "Destination directory")
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                },
+
+                new Option<bool>("--overwrite", "Overwrite destination"),
+            };
+
+            command.Handler = CommandHandler.Create<DirectoryInfo, DirectoryInfo, bool, IHost>(ConvertCommand.Execute);
+
+            return new CommandLineBuilder(command)
+                .UseHost(_ => Host.CreateDefaultBuilder())
+                .UseDefaults()
+                .Build()
+                .InvokeAsync(args);
+        }
+    }
+}
